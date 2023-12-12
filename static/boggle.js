8823bd6c5ac08625c9138a2c6c13db4e3dbfa19e
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-12-12 11:04:08 -0500

    debugged code

--- static/boggle.js
+++ static/boggle.js
@@ -1,31 +1,42 @@
 class BoggleGame {
-  constructor(boardId, seconds = 120) {
-    this.secs = secs;
+  /* make a new game at this DOM id */
+
+  constructor(boardId, secs = 60) {
+    this.secs = secs; // game length
     this.showTimer();
 
     this.score = 0;
     this.words = new Set();
     this.board = $("#" + boardId);
 
+    // every 1000 msec, "tick"
     this.timer = setInterval(this.tick.bind(this), 1000);
 
     $(".add-word", this.board).on("submit", this.handleSubmit.bind(this));
   }
 
+  /* show word in list of words */
+
   showWord(word) {
     $(".words", this.board).append($("<li>", { text: word }));
   }
+
+  /* show score in html */
 
   showScore() {
     $(".score", this.board).text(this.score);
   }
 
+  /* show a status message */
+
   showMessage(msg, cls) {
     $(".msg", this.board).text(msg).removeClass().addClass(`msg ${cls}`);
   }
 
-  async handleSubmit(event) {
-    event.preventDefault();
+  /* handle submission of word: if unique and valid, score & show */
+
+  async handleSubmit(evt) {
+    evt.preventDefault();
     const $word = $(".word", this.board);
 
     let word = $word.val();
@@ -36,28 +47,30 @@ class BoggleGame {
       return;
     }
 
-    const response = await axios.get("/check-word", { params: { word: word } });
-    if (response.data.result === "not-word") {
-      this.showMessage(
-        `${word} is not a valid word in the English dictionary`,
-        "err"
-      );
-    } else if (response.data.result === "not-on-board") {
-      this.showMessage(`${word} is not a valid word on this game board`, "err");
+    // check server for validity
+    const resp = await axios.get("/check-word", { params: { word: word } });
+    if (resp.data.result === "not-word") {
+      this.showMessage(`${word} is not a valid English word`, "err");
+    } else if (resp.data.result === "not-on-board") {
+      this.showMessage(`${word} is not a valid word on this board`, "err");
     } else {
       this.showWord(word);
       this.score += word.length;
       this.showScore();
       this.words.add(word);
-      this.showMessage(`Added word: ${word}`, "ok");
+      this.showMessage(`Added: ${word}`, "ok");
     }
 
     $word.val("").focus();
   }
 
+  /* Update timer in DOM */
+
   showTimer() {
     $(".timer", this.board).text(this.secs);
   }
+
+  /* Tick: handle a second passing in game */
 
   async tick() {
     this.secs -= 1;
@@ -69,10 +82,12 @@ class BoggleGame {
     }
   }
 
+  /* end of game: score and update message. */
+
   async scoreGame() {
     $(".add-word", this.board).hide();
-    const response = await axios.post("/post-score", { score: this.score });
-    if (response.data.brokeRecord) {
+    const resp = await axios.post("/post-score", { score: this.score });
+    if (resp.data.brokeRecord) {
       this.showMessage(`New record: ${this.score}`, "ok");
     } else {
       this.showMessage(`Final score: ${this.score}`, "ok");
