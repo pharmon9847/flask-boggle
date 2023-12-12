46bbe01e2c97b291f396d1202c8d8b91f8b9e975
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-12-12 11:26:38 -0500

    pushed debugged code

--- test.py
+++ test.py
@@ -6,50 +6,48 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
-    
     def setUp(self):
-        """What to do before every test is run"""
-        
+        """Stuff to do before every test."""
+
         self.client = app.test_client()
         app.config['TESTING'] = True
-        
+
     def test_homepage(self):
-        """Test if information is in session and proper HTML is being displayed"""
-        
+        """Make sure information is in the session and HTML is displayed"""
+
         with self.client:
             response = self.client.get('/')
             self.assertIn('board', session)
             self.assertIsNone(session.get('highscore'))
             self.assertIsNone(session.get('nplays'))
-            self.assertIn(b'<p>HighScore:', response.data)
+            self.assertIn(b'High Score:', response.data)
             self.assertIn(b'Score:', response.data)
-            self.assertIn(b'Seconds Remaining:', response.data)
-            
+            self.assertIn(b'Seconds Left:', response.data)
+
     def test_valid_word(self):
-        """Test if chosen word is valid by modifying board in session"""
-        
+        """Test if word is valid by modifying the board in the session"""
+
         with self.client as client:
             with client.session_transaction() as sess:
-                sess['board'] = [["B", "E", "E", "R", "Z"],
-                                 ["B", "E", "E", "R", "T"],
-                                 ["B", "E", "E", "R", "P"],
-                                 ["B", "E", "E", "R", "W"],
-                                 ["B", "E", "E", "R", "M"],]
-        response = self.client.get('/check-word?word=beer')
+                sess['board'] = [["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"], 
+                                 ["C", "A", "T", "T", "T"]]
+        response = self.client.get('/check-word?word=cat')
         self.assertEqual(response.json['result'], 'ok')
-        
-    def test_invalid_word(self):
-        """Test to see if word is on board"""
-        
-        self.client.get('/')
-        response = self.client.get('/check-word?word=salad')
-        self.assertEqual(response.json['result'], 'not-on-board')
-        
-    def test_non_english_word(self):
-        """Test to see if word is in dictionary"""
-        
-        self.client.get('/')
-        response = self.client.get('/check-word?word=nanoonanoo')
-        self.assertEqual(response.json['result'], 'not-word')
 
+    def test_invalid_word(self):
+        """Test if word is in the dictionary"""
+
+        self.client.get('/')
+        response = self.client.get('/check-word?word=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')
+
+    def test_non_english_word(self):
+        """Test if word is on the board"""
+
+        self.client.get('/')
+        response = self.client.get(
+            '/check-word?word=fsjdakfkldsfjdslkfjdlksf')
+        self.assertEqual(response.json['result'], 'not-word')
